ae233c585cada8599f05de6c95dcc2b37480c992
protoeloka <oobiora22@gmail.com>
2024-09-03 15:46:11 -0400

    update

--- assembly/index.ts
+++ assembly/index.ts
@@ -8,7 +8,31 @@ import { console } from "metashrew-as/assembly/utils/logging";
 import { toRLP, RLPItem } from "metashrew-as/assembly/utils/rlp";
 
 import { Protorune } from "protorune/assembly/indexer/index";
-import { NumberingMixin } from "quorum/assembly/indexer/numbering/index";
+import { MessageContext } from "protorune/assembly/indexer/protomessage/MessageContext";
+import { 
+  NumberingMixin, 
+  NumberingMixinProtocol, 
+  NumberingProtoburn, 
+  NumberingProtostone, 
+  NumberingRunestone, 
+  RuneSource 
+} from "quorum/assembly/indexer/numbering/index";
+
+export class AlkaneMessageContext extends MessageContext {
+  handle(): boolean {
+    //TODO: implement handle()
+    return true
+  }
+}
+
+class AlkaneIndex {
+
+}
+
+//TODO: figure out how to define the class
+export class Alkane<T extends AlkaneMessageContext> extends AlkaneIndex {
+
+}
 
 export function _start(): void {
   const data = input();
