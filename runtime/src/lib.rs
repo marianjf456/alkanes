dca12fc25d7ad5618bc00aaad884299edab71ffa
kungfuflex <judoflexchop@gmail.com>
2024-09-04 16:04:14 +0000

    update bindings

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -6,9 +6,45 @@ use std::collections::HashMap;
 use std::fmt::Write;
 use std::panic;
 use std::sync::Arc;
+use wasmi;
+use core::ffi;
 
 #[no_mangle]
-pub extern "C" fn _start() -> () {}
+pub extern "C" fn __wasmi_engine_new() -> *mut wasmi::Engine {
+  let mut config = wasmi::Config::default();
+  config.consume_fuel(true);
+  Box::leak(Box::new(wasmi::Engine::new(&config))) as *mut wasmi::Engine
+}
 
 #[no_mangle]
-pub extern "C" fn _test() -> () {}
+pub extern "C" fn __wasmi_engine_free(ptr: *mut wasmi::Engine) -> () {
+  unsafe {
+    let _ = Box::from_raw(ptr);
+  }
+}
+
+pub struct State {
+  limiter: wasmi::StoreLimits,
+  context: *mut core::ffi::c_void
+}
+
+#[no_mangle]
+pub extern "C" fn __wasmi_store_new(engine: *mut wasmi::Engine, context: *mut core::ffi::c_void, memory_limit: usize, fuel_limit: u64) -> *mut wasmi::Store<State> {
+  let mut store = wasmi::Store::<State>::new(
+    unsafe { &*engine },
+    State {
+      context,
+      limiter: wasmi::StoreLimitsBuilder::new().memory_size(memory_limit).build(),
+    }
+  );
+  store.limiter(|state| &mut state.limiter);
+  wasmi::Store::<State>::set_fuel(&mut store, fuel_limit).unwrap();
+  Box::leak(Box::new(store)) as *mut wasmi::Store<State>
+}
+
+#[no_mangle]
+pub extern "C" fn __wasmi_store_free(ptr: *mut wasmi::Store<State>) -> () {
+  unsafe {
+    let _ = Box::from_raw(ptr);
+  }
+}
