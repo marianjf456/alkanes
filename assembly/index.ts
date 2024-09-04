ac969532f0af4b85c05625a01ea12e8593ba4595
kungfuflex <judoflexchop@gmail.com>
2024-09-04 02:12:17 +0000

    add numbering

--- assembly/index.ts
+++ assembly/index.ts
@@ -25,8 +25,73 @@ export class AlkaneMessageContext extends MessageContext {
   }
 }
 
-class AlkaneIndex extends Protorune<AlkaneMessageContext> {
+function expandToNumberingAlign(
+  v: Array<Protostone>,
+  tx: RunesTransaction,
+): Array<Protostone> {
+  const result = new Array<Protostone>(0);
+  for (let i = 0; i < v.length; i++) {
+    result.push(NumberingProtostone.fromProtocolMessage(v[i], tx).unwrap());
+  }
+  return result;
+}
 
+class ProtostoneReduce {
+  tx: RunesTransaction;
+  stones: Array<NumberingProtostone> = new Array<NumberingProtostone>();
+  constructor(tx: RunesTransaction) {
+    this.tx = tx;
+  }
+}
+
+export class AlkaneIndex extends Protorune<AlkaneMessageContext> {
+  processRunestone(
+    block: RunesBlock,
+    tx: RunesTransaction,
+    txid: ArrayBuffer,
+    height: u32,
+    i: u32,
+  ): RunestoneMessage {
+    const baseRunestone = tx.runestone();
+    if (changetype<usize>(baseRunestone) === 0)
+      return changetype<RunestoneMessage>(0);
+    const runestone = NumberingRunestone.fromProtocolMessage(baseRunestone, tx);
+    const balancesByOutput = changetype<Map<u32, ProtoruneBalanceSheet>>(
+      runestone.process(tx, txid, height, i),
+    );
+    const protostones = Protostone.from(runestone.unwrap()).protostones(
+      tx.outs.length + 1,
+    );
+    const _burns = protostones.burns();
+    const burns = new Array<NumberingProtoburn<NumberingRunestone>>();
+
+    for (let i = 0; i < _burns.length; i++) {
+      burns.push(NumberingProtoburn.fromBurn(_burns[i], runestone));
+    }
+
+    const runestoneOutputIndex = tx.runestoneOutputIndex();
+    const edicts = Edict.fromDeltaSeries(runestone.edicts);
+
+    if (burns.length > 0) {
+      this.processProtoburns(
+        baseRunestone.unallocatedTo,
+        balancesByOutput,
+        txid,
+        runestoneOutputIndex,
+        Protostone.from(runestone.unwrap()),
+        edicts,
+        burns,
+      );
+    }
+    const stones = protostones.flat().reduce((reduce, stone) => {
+      reduce.stones.push(
+        NumberingProtostone.fromProtocolMessage(stone, reduce.tx),
+      );
+      return reduce;
+    }, new ProtostoneReduce(tx)).stones;
+    this.processProtostones(stones, block, height, tx, txid, i);
+    return runestone;
+  }
 }
 
 export function _start(): void {
