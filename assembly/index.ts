be5fc9ebd0b8521d55e31301588c4adef001d037
kungfuflex <judoflexchop@gmail.com>
2024-09-10 16:26:58 +0000

    updates

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,23 +1,5 @@
-import { Box } from "metashrew-as/assembly/utils/box";
-import { _flush, input } from "metashrew-as/assembly/indexer/index";
-import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
-import { Block } from "metashrew-as/assembly/blockdata/block";
-import { AlkaneIndex } from "./indexer";
-
-import { console } from "metashrew-as/assembly/utils/logging";
-
 export function __execute(): i32 {
   // console.log(Box.from(env.block).toHexString());
 
   return 0;
-}
-
-export function _start(): void {
-  const data = input();
-  const box = Box.from(data);
-  const height = parsePrimitive<u32>(box);
-  const block = new Block(box);
-  new AlkaneIndex().indexBlock(height, block);
-  console.log("got block " + height.toString(10));
-  _flush();
 }
--- assembly/indexer/index.ts
+++ assembly/indexer/index.ts
@@ -1,1 +1,16 @@
-export * from "./alkane";
+import { Box } from "metashrew-as/assembly/utils/box";
+import { Block } from "metashrew-as/assembly/blockdata/block";
+import { console } from "metashrew-as/assembly/utils/logging";
+import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
+import { input, _flush } from "metashrew-as/assembly//indexer";
+import { AlkaneIndex } from "./alkane";
+
+export function _start(): void {
+  const data = input();
+  const box = Box.from(data);
+  const height = parsePrimitive<u32>(box);
+  const block = new Block(box);
+  new AlkaneIndex().indexBlock(height, block);
+  console.log("got block " + height.toString(10));
+  _flush();
+}
