401abfc6b4c9d25dbc5cfe14117c4faecbc46ceb
v16s <16210686+v16s@users.noreply.github.com>
2024-09-10 19:41:34 +0530

    fix prettier

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,9 @@
-import { env } from "./env";
 import { Box } from "metashrew-as/assembly/utils/box";
 
+export function __start(): void {}
+
 export function __execute(): i32 {
-  console.log(Box.from(env.block).toHexString());
+  // console.log(Box.from(env.block).toHexString());
 
   return 0;
 }
