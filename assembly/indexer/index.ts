fee9175fa36e3ad9d093486384e1249a402e7b30
clothic <hello.cryptologic.llc@gmail.com>
2024-09-14 16:38:49 -0500

    add spendablesindex index

--- assembly/indexer/index.ts
+++ assembly/indexer/index.ts
@@ -4,12 +4,21 @@ import { console } from "metashrew-as/assembly/utils/logging";
 import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
 import { input, _flush } from "metashrew-as/assembly//indexer";
 import { AlkaneIndex } from "./AlkaneIndex";
+import { SpendablesIndex } from "metashrew-spendables/assembly/indexer";
+import { GENESIS } from "metashrew-runes/assembly/indexer/constants";
 
 export function _start(): void {
   const data = input();
   const box = Box.from(data);
   const height = parsePrimitive<u32>(box);
+  if (height < GENESIS - 6) {
+    _flush();
+    return;
+  }
   const block = new Block(box);
+  if (height >= GENESIS) {
+    new SpendablesIndex().indexBlock(height, block);
+  }
   new AlkaneIndex().indexBlock(height, block);
   console.log("got block " + height.toString(10));
   _flush();
