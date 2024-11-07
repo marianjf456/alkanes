2344882d399bca7076d84fbfb3257b9052c26872
phuncti0n <zmill777@proton.me>
2024-11-07 13:09:42 -0500

    blueprint

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,2 +1,12 @@
 export declare const STUB = "";
+export declare const createProtoStone: ({ runeName, mintAmount, divisibility, premine, cap }: {
+    runeName: string;
+    mintAmount: number;
+    premine: number;
+    divisibility: number;
+    cap: number;
+}) => {
+    encodedRunestone: Buffer;
+    etchingCommitment?: Buffer;
+};
 //# sourceMappingURL=util.d.ts.map
--- src.ts/util.ts
+++ src.ts/util.ts
@@ -1,1 +1,71 @@
+import { encodeRunestoneProtostone } from "protorune/lib/src.ts/runestone_protostone_upgrade.js";
+import { ProtoStone } from "protorune/lib/src.ts/protostone.js";
+import * as bitcoin from "bitcoinjs-lib";
+
 export const STUB = "";
+
+export const assembleTx = ({
+  feeUtxos,
+  toAddress,
+  network,
+  amount,
+}: {
+  feeUtxos;
+  toAddress: string;
+  network: bitcoin.Network;
+  amount: number;
+}) => {
+  let tx = new bitcoin.Psbt({ network });
+  const script = createRuneEtchScript({
+    runeName,
+    perMintAmount: amount,
+    divisibility,
+    pointer: 1,
+  });
+  const output = { script: script, value: 0 };
+  tx.addOutput(output);
+};
+
+export const createRuneEtchScript = ({
+  pointer = 0,
+  runeName,
+  symbol,
+  divisibility,
+  perMintAmount,
+  premine = 0,
+  cap,
+  turbo,
+}: {
+  pointer?: number;
+  runeName: string;
+  symbol: string;
+  divisibility?: number;
+  perMintAmount: number;
+  cap?: number;
+  premine?: number;
+  turbo?: boolean;
+}) => {
+  const runeEtch = encodeRunestoneProtostone({
+    etching: {
+      divisibility,
+      premine: BigInt(premine),
+      runeName,
+      symbol,
+      terms: {
+        cap: cap && BigInt(cap),
+        amount: perMintAmount && BigInt(perMintAmount),
+      },
+      turbo,
+    },
+    pointer,
+    protostones: [
+      {
+        protocolTag: 64n,
+        burn: {
+          pointer: 1,
+        },
+      },
+    ].map((v) => new ProtoStone(v)),
+  }).encodedRunestone;
+  return runeEtch;
+};
