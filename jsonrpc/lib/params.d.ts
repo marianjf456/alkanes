74d157094a82d1195df1ec41ff444d7251278d7d
kungfuflex <judoflexchop@gmail.com>
2025-01-16 14:49:47 -0600

    working esplora

--- jsonrpc/lib/params.d.ts
+++ jsonrpc/lib/params.d.ts
@@ -3,5 +3,7 @@ export declare const RPCPASSWORD: string;
 export declare const RPCAUTH: string;
 export declare const METASHREW_URI: string;
 export declare const ORD_PORT: number;
+export declare const ESPLORA_PORT: number;
 export declare const ORD_HOST: string;
+export declare const ESPLORA_HOST: string;
 export declare const DAEMON_RPC_ADDR: string;
--- jsonrpc/src.ts/params.ts
+++ jsonrpc/src.ts/params.ts
@@ -8,7 +8,9 @@ export const METASHREW_URI =
   process.env.METASHREW_URI || "http://metashrew-view:8080";
 
 export const ORD_PORT = process.env.ORD_PORT && Number(process.env.ORD_PORT) || 8090;
+export const ESPLORA_PORT = process.env.ESPLORA_PORT && Number(process.env.ESPLORA_PORT) || 50010;
 
 export const ORD_HOST = process.env.ORD_HOST || "ord";
+export const ESPLORA_HOST = process.env.ESPLORA_HOST || "esplora";
 
 export const DAEMON_RPC_ADDR = process.env.DAEMON_RPC_ADDR || '127.0.0.1:18443';
