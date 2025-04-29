b1c9e697a38f3ea8c721618237bf1d90d05490b1
clothic <hello.cryptologic.llc@gmail.com>
2025-04-29 02:38:31 +0000

    fix query simulate script

--- scripts/query-jsonrpc.js
+++ scripts/query-jsonrpc.js
@@ -1,25 +1,26 @@
 import { AlkanesRpc } from "../lib/rpc.js";
 
-const alkanes = new AlkanesRpc('https://mainnet.sandshrew.io/v2/lasereyes');
+// const alkanes = new AlkanesRpc({baseUrl: 'https://mainnet.sandshrew.io/v2/lasereyes'});
+const alkanes = new AlkanesRpc({ baseUrl: "http://localhost:18899" });
 
 // Query protorunes balance
 const balance = await alkanes.protorunesbyaddress({
-  address: 'bc1pr8vjq0fk89f5sw3r4n9scrasvw7kaud9akhzw57c3ygycsjspvvseyjcma',
+  address: "bc1pr8vjq0fk89f5sw3r4n9scrasvw7kaud9akhzw57c3ygycsjspvvseyjcma",
   protocolTag: 1n,
 });
 
-console.log('Protorunes balance:', balance);
+console.log("Protorunes balance:", balance);
 
 // Simulate transaction
 const simulation = await alkanes.simulate({
   alkanes: [],
-  block: '',
-  transaction: '',
+  block: "",
+  transaction: "",
   height: 1000000n,
   txindex: 0,
   target: {
     block: 2n,
-    tx: 465n,
+    tx: 460n,
   },
   inputs: [99n],
   pointer: 0,
@@ -27,4 +28,4 @@ const simulation = await alkanes.simulate({
   vout: 0,
 });
 
-console.log('Simulation result:', simulation);
+console.log("Simulation result:", simulation);
